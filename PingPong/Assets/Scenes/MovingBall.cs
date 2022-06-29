fde8e8c3603098d556b981ec6567d4e48cf41813
YuJin Jeon <83286706+Yujin0827@users.noreply.github.com>
2022-06-29 18:03:24 +0900

    change

--- PingPong/Assets/Scenes/MovingBall.cs
+++ PingPong/Assets/Scenes/MovingBall.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingBall : MonoBehaviour
 {
+    [SerializeField] private GameObject GazePointer;
+    [SerializeField] private GameObject Ball;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class MovingBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pointer = GazePointer.transform.position;
+        GazePointer.transform.position = new Vector3(Ball.transform.position.x, pointer.y, pointer.z);
+
+        if (Vector3.Distance(GazePointer.transform.position, Ball.transform.position) <= 0.1f)
+        {
+            Ball.transform.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        else
+        {
+            Ball.transform.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+
+
     }
 }
